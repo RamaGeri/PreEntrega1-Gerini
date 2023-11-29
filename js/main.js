a39bc4dafe74ca136afd08ede47a9b00419f5336
RamaGeri <ramironicolasgerini@gmail.com>
2023-11-29 19:48:38 -0300

    creado de clases constructoras con metodos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//register de usario y contraseña
+/* //register de usario y contraseña
 const USER_REGISTER = prompt("Registre un nombre de usuario")
 const PASSWORD_REGISTER = prompt("Registre una contraseña")
 
@@ -19,48 +19,93 @@ do {
     const PASSWORD_LOGIN = prompt("Ingrese su contraseña");
 
     if (((USER_LOGIN === "admin") && (PASSWORD_LOGIN === "admin")) || ((USER_LOGIN === USER_REGISTER) && (PASSWORD_LOGIN === PASSWORD_REGISTER))){
-        alert("Bienvenido " + USER_LOGIN + "!");
-        console.log("Bienvenido " + USER_LOGIN + "!")
+        alert("Felicidades, ahora necesitamos conocerte!");
+        console.log("Iniciaste sesion correctamente, ahora necesitamos conocerte!")
         repetir = false;
     }else{
         alert("El usuario o contraseña es incorrecto :c");
         repetir = true;
     }
-}while (repetir === true);
+}while (repetir === true); */
 
-
-// Carrito de compras de productos
-let sumaProductos = 0;
-let cantidad = 0;
-let continuar;
-
-function agregarProducto(nombre, precio) {
-    sumaProductos += precio;
-    cantidad += 1;
-    continuar = prompt("¿Queres agregar algo más? SI / NO").toLowerCase();
-    console.log("Producto: " + nombre);
+// Saludo al cliente
+class Cliente {
+    constructor(nombre, apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    darMensaje() {
+        return `¡Bienvenido, ${this.nombre} ${this.apellido}! \nQue vas a pedir hoy?`;
+    }
 }
 
+let nombre = prompt("Ingresa tu nombre:");
+let apellido = prompt("Ingresa tu apellido:");
+
+let mensajeBienvenida = new Cliente(nombre, apellido);
+
+alert(mensajeBienvenida.darMensaje());
+console.log(mensajeBienvenida.darMensaje()); 
+
+// Carrito de compras
+class Carrito {
+    constructor() {
+        this.productos = [];
+    }
+
+    agregarProducto(producto, precio) {
+        this.productos.push({ producto, precio });
+    }
+
+    calcularTotal() {
+        return this.productos.reduce((total, producto) => total + producto.precio, 0);
+    }
+}
+
+let carrito = new Carrito();
+
 do {
-    let pedido = parseInt(prompt("¿Qué vas a pedir? (Elegí un número) 1) Hamburguesa con queso -1400$- 2) Papas fritas -800$- 3) Gaseosa -500$- 4) Agua -400$-"));
+    let pedido = parseInt(prompt("¿Qué vas a querer? (Elegi un número) 1) Hamburguesa con queso - 1400$ 2) Papas fritas - 800$ 3) Gaseosa - 500$ 4) Agua - 400$"));
 
     switch (pedido) {
         case 1:
-            agregarProducto("Hamburguesa con queso", 1400);
+            carrito.agregarProducto("Hamburguesa con queso", 1400);
             break;
         case 2:
-            agregarProducto("Papas fritas", 800);
+            carrito.agregarProducto("Papas fritas", 800);
             break;
         case 3:
-            agregarProducto("Gaseosa", 500);
+            carrito.agregarProducto("Gaseosa", 500);
             break;
         case 4:
-            agregarProducto("Agua", 400);
+            carrito.agregarProducto("Agua", 400);
             break;
         default:
             alert("Ingresa un número válido");
-            continuar = "si";
+            break;
     }
-} while (continuar === "si");
 
-alert("Tu compra es un total de " + sumaProductos + "$ por " + cantidad + " producto/s");
+    let continuar = prompt("¿Quieres agregar otro producto? SI / NO").toLowerCase();
+    if (continuar === "no") {
+        break;
+    }
+} while (true);
+
+//Mensajes
+
+let mensajePedido = "Tu pedido:\n";
+
+carrito.productos.forEach(producto => {
+    mensajePedido += `- ${producto.producto}: ${producto.precio}$\n`;
+});
+
+alert(mensajePedido);
+
+alert(`El total de tu compra es: ${carrito.calcularTotal()}$`);
+
+console.log("Tu pedido:");
+carrito.productos.forEach(producto => {
+    console.log(`- ${producto.producto}: ${producto.precio}$`);
+});
+
+console.log(`Total: ${carrito.calcularTotal()}$`);
